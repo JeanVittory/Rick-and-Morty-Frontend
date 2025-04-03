9f852052f8a11019312a6d9b128c2c2198f87287
JeanVittory <vittory.dev@gmail.com>
2025-04-03 11:05:04 -0500

    feat: :wrench: Add global colors and typography using tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx,ts}"],
+  content: ['./src/**/*.{html,js,tsx,ts}'],
   theme: {
     extend: {
-      fontFamily:{
-        nunito: ["Nunito"]
+      fontFamily: {
+        nunito: ['Nunito'],
+        greycliff: ['Greycliff', 'sans-serif'], // Usará los pesos definidos en @font-face
+      },
+      fontWeight: {
+        thin: 100,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
       },
       screens: {
-        xl: "1281px",
+        xl: '1281px',
+      },
+      colors: {
+        lavender: '#EEE3FF',
+        'purple-primary': '#8054C7',
+        'purple-dark': '#5A3696',
+        'green-primary': '#63D838',
       },
     },
   },
   plugins: [],
-}
+};
