6d8e5e0639428a652c5c3613141ae226a19240a1
JeanVittory <vittory.dev@gmail.com>
2025-04-03 11:06:14 -0500

    feat: :sparkles: Updating React Router with new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { reduxStore } from "./redux/store";
-import { Header } from "./components/Header.components";
-import { AllCharactersContainer } from "./utilities/LazyComponents";
-import { SingleCharacterContainer } from "./utilities/LazyComponents";
-import { Suspense } from "react";
-import { Spinner } from "./components/Spinner";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { reduxStore } from './redux/store';
+import { AllCharactersContainer } from './utilities/LazyComponents';
+
+import { Suspense } from 'react';
+import { Spinner } from './components/Spinner';
 
 function App() {
   return (
     <div>
       <Provider store={reduxStore}>
         <Router>
-          <Header />
           <Suspense fallback={<Spinner />}>
             <Routes>
-              <Route path="/" element={<AllCharactersContainer />} />
-              <Route
-                path="character/:id"
-                element={<SingleCharacterContainer />}
-              />
+              <Route path='/' element={<AllCharactersContainer />} />
             </Routes>
           </Suspense>
         </Router>
