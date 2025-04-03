573791b7cb50aa23136cece060243654f0d77a8f
JeanVittory <vittory.dev@gmail.com>
2025-04-03 11:08:49 -0500

    feat: :art: Formatting global Spinner with Prettier

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,17 +1,10 @@
-import { SpinnerRoundOutlined } from "spinners-react";
+import { SpinnerRoundOutlined } from 'spinners-react';
 
 export const Spinner = () => {
   return (
     <>
-      <div className="bg-[#fdfcdc] w-screen h-screen flex justify-center items-center">
-        {
-          <SpinnerRoundOutlined
-            size={50}
-            thickness={100}
-            speed={100}
-            color="#36ad47"
-          />
-        }
+      <div className='w-screen h-screen flex justify-center items-center'>
+        {<SpinnerRoundOutlined size={50} thickness={100} speed={100} color='#36ad47' />}
       </div>
     </>
   );
