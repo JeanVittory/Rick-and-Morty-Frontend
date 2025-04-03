cee4e579569e51d8be0f3a076050272f90d46b5e
JeanVittory <vittory.dev@gmail.com>
2025-04-03 11:09:28 -0500

    feat: :sparkles: Add new hook to check the viewportsize

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      const isViewportUnder500px = window.innerWidth < 768;
+
+      setIsMobile(isViewportUnder500px);
+    };
+
+    checkIsMobile();
+
+    window.addEventListener('resize', checkIsMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
+  }, []);
+
+  return isMobile;
+};
