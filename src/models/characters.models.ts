9139641276b111b4583c95c184dd72f85faf46ab
JeanVittory <vittory.dev@gmail.com>
2025-04-03 11:10:22 -0500

    feat: :sparkles: Add new types about characters and comments

--- src/models/characters.models.ts
+++ src/models/characters.models.ts
@@ -1,3 +1,5 @@
+import { ChangeEvent } from 'react';
+
 export type Characters = {
   id: number;
   name: string;
@@ -16,4 +18,51 @@ export type CharactersData = {
     };
     results: Characters[];
   };
+};
+
+export type SelectedCharacter = {
+  id: number;
+  name: string;
+  image: string;
+  species: string;
+  status: string;
+  type: string;
+};
+
+export type AllCharacterType = {
+  dataFromRedux: {
+    data: Characters[];
+  };
+  visibleItems: Characters[];
+  selectedCharacter: Characters | null;
+  handleCharacterSelect: (character: Characters) => void;
+  favoriteCharacters: Characters[];
+  toggleFavorite: (character: Characters) => void;
+  isFetching: boolean;
+};
+
+export type StarredCharactersType = {
+  selectedCharacter: Characters | null;
+  handleCharacterSelect: (character: Characters) => void;
+  favoriteCharacters: Characters[];
+  toggleFavorite: (character: Characters) => void;
+};
+
+export type CharacterFilterType = {
+  showFilterModal: boolean;
+  toggleFilterModal: () => void;
+  searchTerm: string;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  statusFilter: string;
+  handleFilterChange: (filterType: string, value: string) => void;
+  speciesFilter: string;
+  applyFilters: () => void;
+  handleChangeOrder: (e: string) => void;
+  sortOrder: string;
+};
+
+export type SearchBarCharacterFilterType = {
+  searchTerm: string;
+  handleSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  toggleFilterModal: () => void;
 };
---
+++ src/models/comments.models.ts
@@ -0,0 +1,1 @@
+export type CommentsType = { text: string; date: string; characterId: number };
